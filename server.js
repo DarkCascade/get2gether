2297a7e96290e72a58faf0d8aabe1e4c536070fc
James Donnell <darkcascade@gmail.com>
2015-11-10 13:34:58 -0700

    making things all caps

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var mean = require('meanio');
 
 // Creates and serves mean application
 mean.serve({ /*options placeholder*/ }, function(app, config) {
-  console.log('Mean app started on port ' + config.http.port + ' (' + process.env.NODE_ENV + ')');
+  console.log('MEAN app started on port ' + config.http.port + ' (' + process.env.NODE_ENV + ')');
   if(config.https && config.https.port){
-    console.log('Mean secure app started on port ' + config.https.port + ' (' + process.env.NODE_ENV + ')');
+    console.log('MEAN secure app started on port ' + config.https.port + ' (' + process.env.NODE_ENV + ')');
   }
 });
